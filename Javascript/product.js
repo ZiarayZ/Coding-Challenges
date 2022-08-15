e49ce61d2e6c32acab0c13496ac22cb11d78d197
ZiarayZ <jakesmith@binaryrage.com>
2022-08-15 11:58:07 +0100

    update repo

---
+++ Javascript/product.js
@@ -0,0 +1,10 @@
+//Given an array of distinct elements and a number x, find if there is a pair with product equal to x.
+
+function product(value, array) {
+
+    return [0, 0];
+}
+
+var x = 45;
+const values = [1,5,15,18,46,9];
+console.log(product(x, values));
