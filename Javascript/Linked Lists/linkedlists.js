b91a31fe334ea4bef8ff38a80d494ddf94a82f30
ZiarayZ <jakesmith@binaryrage.com>
2022-08-15 17:21:30 +0100

    untested version of doubly
    -also hopefully fixed the remove method

--- Javascript/Linked Lists/linkedlists.js
+++ Javascript/Linked Lists/linkedlists.js
@@ -4,27 +4,63 @@ class SLLNode {
         this.next;
     }
 
-    getNode(index) {
+    getNextNode(index) {
         if (index > 0) {
-            return this.next.getNode(index-1);
+            return this.next.getNextNode(index-1);
         }
         return this;
     }
 }
 
-class DLLNode {
+class DLLNode extends SLLNode {
     constructor(value) {
-        this.value = value;
+        super(value);
         this.prev;
-        this.next;
+    }
+
+    getPrevNode(index) {
+        if (index > 0) {
+            return this.prev.getPrevNode(index-1);
+        }
+        return this;
     }
 }
 
-class SinglyLinkedList {
+class LinkedList {
     constructor() {
         this.count = 0;
         this.head;
         this.tail;
+    }
+
+    get popFront() {
+        let previous = this.head;
+        this.head = this.head.next;
+        this.count--;
+        return previous.value;
+    }
+
+    contains(item) {
+        for (let i = 0; i < this.count; i++) {
+            if (this.getNode(i).value === item) {
+                return true;//could also use a integer location instead
+            }
+        }
+        return false;//which would mean returning -1 here
+    }
+
+    get values() {
+        let arr = [];
+        for (let i = 0; i < this.count; i++) {
+            arr[i] = this.getNode(i).value;
+        }
+        return arr;
+    }
+}
+
+class SinglyLinkedList extends LinkedList {
+    constructor() {
+        super();
     }
 
     pushBack(value) {
@@ -42,6 +78,7 @@ class SinglyLinkedList {
             this.tail = this.tail.next;
             return true;
         }
+        this.count--;
         return false;
     }
     pushFront(value) {
@@ -60,6 +97,7 @@ class SinglyLinkedList {
             this.head.next = second;
             return true;
         }
+        this.count--;
         return false;
     }
 
@@ -71,12 +109,6 @@ class SinglyLinkedList {
         this.count--;
         return last.value;
     }
-    get popFront() {
-        let prev = this.head;
-        this.head = this.head.next;
-        this.count--;
-        return prev.value;
-    }
 
     remove(index) {
         if (index >= this.count || index < 0) {
@@ -86,50 +118,130 @@ class SinglyLinkedList {
             this.count--;
             return true;
         } else if (index > 0) {
+            let item = this.getNode(index-1);
+            item.next = item.next.next;
+            this.count--;
+            return true;
+        }
+
+        return false;//change to throw type error
+    }
+
+    getNode(index) {
+        if (index >= this.count || index < 0) {
+            return -1;//change to throw error
+        }
+        return this.head.getNextNode(index);
+    }
+}
+
+class DoublyLinkedList extends LinkedList {
+    constructor() {
+        super();
+    }
+
+    pushBack(value) {
+        this.count++;
+        if (this.count === 1) {
+            this.head = new DLLNode(value);
+            this.tail = this.head;
+            return true;
+        } else if (this.count === 2) {
+            this.tail = new DLLNode(value);
+            this.tail.prev = this.head;
+            this.head.next = this.tail;
+            return true;
+        } else {
+            this.tail.next = new DLLNode(value);
+            this.tail.next.prev = this.tail;
+            this.tail = this.tail.next;
+            return true;
+        }
+        this.count--;
+        return false;
+    }
+    pushFront(value) {
+        this.count++;
+        if (this.count === 1) {
+            this.head = new DLLNode(value);
+            this.tail = this.head;
+            return true;
+        } else if (this.count === 2) {
+            this.head = new DLLNode(value);
+            this.head.next = this.tail;
+            this.tail.prev = this.head;
+            return true;
+        } else {
+            this.head.prev = new DLLNode(value);
+            this.head.prev.next = this.head;
+            this.head = this.head.prev;
+            return true;
+        }
+        this.count--;
+        return false;
+    }
+    
+    get popBack() {
+        let following = this.tail;
+        this.tail = this.tail.prev;
+        this.count--;
+        return following.value;
+    }
+
+    remove(index) {
+        if (index >= this.count || index < 0) {
+            return false;//change to throw index error
+        } else if (index === 0) {
+            this.head = this.head.next;
+            this.count--;
+            return true;
+        } else if (index === this.count-1) {
+            this.tail = this.tail.prev;
+            this.count--;
+            return true;
+        } else if (index > 0) {
             let item = this.getNode(index);
+            item.next.prev = item.prev;
+            item.prev.next = item.next;
             item = item.next;
             this.count--;
             return true;
         }
 
         return false;//change to throw type error
-        //use getNode method to locate index-1
-    }
-
-    contains(item) {
-        for (let i = 0; i < this.count; i++) {
-            if (this.getNode(i).value === item) {
-                return true;//could also use a integer location instead
-            }
-        }
-        return false;//which would mean returning -1 here
     }
 
     getNode(index) {
         if (index >= this.count) {
-            return -1;//change to throw error
+            return -1;//change to throw index error
+        } else if (index < 0) {
+            if (index <= -this.count) {
+                return -1;//change to throw index error
+            } else if (index >= -this.count/2) {
+                return this.tail.getPrevNode(-index);
+            } else if (index <= -this.count/2) {
+                return this.head.getNextNode(1-this.count + index);
+            }
+        } else if (index <= this.count/2) {
+            return this.head.getNextNode(index);
+        } else if (index >= this.count/2) {
+            return this.tail.getPrevNode(this.count-1 - index);
         }
-        return this.head.getNode(index);
-    }
 
-    get values() {
-        let arr = [];
-        for (let i = 0; i < this.count; i++) {
-            arr[i] = this.getNode(i).value;
-        }
-        return arr;
-    }
-}
-
-class DoublyLinkedList {
-    constructor() {
-        this.count;
-        this.head;
-        this.tail;
+        return -1;//throw type error
     }
 }
 
 const linkedlist = new SinglyLinkedList();
+linkedlist.pushBack(4);
+linkedlist.pushBack(3);
+linkedlist.pushBack(2);
+linkedlist.pushBack(1);
+console.log(linkedlist.values);
+
+console.log("");
+
+linkedlist = new DoublyLinkedList();
 linkedlist.pushBack(4);
 linkedlist.pushBack(3);
 linkedlist.pushBack(2);
