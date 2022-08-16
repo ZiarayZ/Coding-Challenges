1bb727834bd37203455ff3479aa6a1fbef85406b
ZiarayZ <jakesmith@binaryrage.com>
2022-08-16 12:07:52 +0100

    finish product
    - theorized another method as well

--- Javascript/Product/product.js
+++ Javascript/Product/product.js
@@ -1,10 +1,20 @@
 //Given an array of distinct elements and a number x, find if there is a pair with product equal to x.
 
-function product(value, array) {
-
+function productNLOGN(value, array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i; j < array.length; j++) {
+            if (array[i] * array[j] == value) {
+                return [i, j];
+            }
+        }
+    }
     return [0, 0];
 }
 
-var x = 45;
-const values = [1,5,15,18,46,9];
-console.log(product(x, values));
+function product(value, array) {
+    // calculate the factors of value, then compare to array
+    return [0, 0];
+}
+
+console.log(productNLOGN(45, [1,5,15,18,46,9]));
+console.log(product(45, [1,5,15,18,46,9]));
