b21ad590c41c3972bead9969c69c816ed99cc436
ZiarayZ <jakesmith@binaryrage.com>
2022-08-16 11:12:07 +0100

    final solution
    - conversion back to original slice type is left to whoever uses the function

--- Golang/Traverse Linked List/traverselinkedlist.go
+++ Golang/Traverse Linked List/traverselinkedlist.go
@@ -37,8 +37,26 @@ func faa(arg interface{}) ([]interface{}, bool) {
 	return arr, true
 }
 
-func traverse(M int, N int, arg []interface{}) []interface{} {
-	return arg
+//actual function used to traverse the "linked list" in this case it's just a slice of anything being used as an example
+func traverse(M int, N int, arg []interface{}) ([]interface{}, bool) {
+	val, ok := isSlice(arg)
+	if !ok {
+		return arg, false
+	}
+	newVal := val.Len()
+	if M+N > newVal {
+		return arg, false
+	}
+
+	newArg := make([]interface{}, newVal-N)
+
+	for i := 0; i < M; i++ {
+		newArg[i] = arg[i] //here would use the linked list get function
+	}
+	for i := M + N; i < newVal; i++ {
+		newArg[i-N] = arg[i] //here would use the linked list get function
+	}
+	return newArg, true
 }
 
 func main() {
@@ -46,7 +64,12 @@ func main() {
 	fmt.Println(slice)
 	newSlice, test := faa(slice)
 	if test {
-		fmt.Println(traverse(1, 4, newSlice))
+		slicedSlice, testOK := traverse(1, 4, newSlice)
+		if testOK {
+			fmt.Println(slicedSlice) //should output "[2, 13, 17]"
+		} else {
+			fmt.Println("Traverse Failed.")
+		}
 	} else {
 		fmt.Println("Conversion Failed.")
 	}
