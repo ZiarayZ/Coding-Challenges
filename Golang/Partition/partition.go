4991fde46f36199244e6f17a9e7c5c31ff347f34
ZiarayZ <jakesmith@binaryrage.com>
2022-08-17 17:01:28 +0100

    simple start

--- Golang/Partition/partition.go
+++ Golang/Partition/partition.go
@@ -1,1 +1,23 @@
 package main
+
+//Given a string A, partition A such that every substring of the partition is a palindrome. Return the minimum cuts needed for a palindrome partitioning of A.
+// attempt: locate largest palindromes, cut and repeat on remaining substrings
+
+import (
+	"fmt"
+	"strings"
+)
+
+func partite(A string) ([]string, int) {
+	//removes whitespace in and between for palindromes
+	A = strings.TrimSpace(A)
+	A = strings.ReplaceAll(A, " ", "")
+	srune := []rune(A)
+	returnSlice := []string{string(srune[0]), string(srune[1:])}
+	return returnSlice, 1
+}
+
+func main() {
+	value, cuts := partite("hollo")
+	fmt.Println(value, cuts)
+}
