3892e811e4cb8e4eb608e07a96fd6d0adb90e54d
ZiarayZ <jakesmith@binaryrage.com>
2022-08-17 10:16:44 +0100

    slight fix
    - simplified interleave method
    - attempted fix of recall method

--- Golang/Z_Order/Z_order.go
+++ Golang/Z_Order/Z_order.go
@@ -13,13 +13,10 @@ import (
 //using unsigned ints for bit operations, 2 16 bits become a 32 bit
 func interleave(coords [2]uint16) uint32 {
 	var num uint32 = 0
-	var base uint16 = 2
-	var output uint16 = 1
 	var i uint16 = 0
 	for i = 0; i < 16; i++ { //loop could be made more efficient later, break after checking all set bits
-		num |= uint32(coords[0]&output) << i
-		num |= uint32(coords[1]&output) << (i + 1)
-		output *= base
+		num |= uint32(coords[0]&(1<<i)) << i
+		num |= uint32(coords[1]&(1<<i)) << (i + 1)
 	}
 	return num
 }
@@ -28,15 +25,15 @@ func recall(value uint32) [2]uint16 {
 	coords := [2]uint16{0, 0}
 	var i uint16 = 0
 	for i = 0; i < 16; i++ {
-		coords[0] |= uint16((value >> (2 * i)) & 1)
-		coords[1] |= uint16((value >> (2*i + 1)) & 1)
+		coords[0] |= uint16((value >> (2 * i)) & (1 << i))
+		coords[1] |= uint16((value >> (2*i + 1)) & (1 << i))
 	}
 	return coords
 }
 
 func main() {
-	slice := [2]uint16{1, 2}
+	slice := [2]uint16{127, 127}
 	fmt.Println(interleave(slice))
-	var number uint32 = 9
+	var number uint32 = 16383
 	fmt.Println(recall(number))
 }
