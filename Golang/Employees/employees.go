6b2810511ea7133e17d2a90cbbe7410b756ca661
ZiarayZ <jakesmith@binaryrage.com>
2022-08-18 14:31:22 +0100

    recursive solution
    - creates multiple O(n^2) loops

--- Golang/Employees/employees.go
+++ Golang/Employees/employees.go
@@ -4,12 +4,32 @@ import (
 	"fmt"
 )
 
-func catch(input map[rune]rune, target rune) rune {
-
+func catch(v rune, m map[rune]rune) []rune {
+	slice := []rune{v}
+	for k, r := range m {
+		if v == r {
+			slice = append(slice, catch(k, m)...)
+		}
+	}
+	return slice
 }
 
 func report(input map[rune]rune) map[rune][]rune {
+	m := make(map[rune][]rune)
 
+	//separate loop for populating map, so there's no chance of overwriting/erasing runes
+	for k := range input {
+		m[k] = []rune{}
+	}
+
+	//write in initial runes
+	for k, r := range input {
+		if k != r {
+			m[r] = append(m[r], catch(k, input)...)
+		}
+	}
+
+	return m
 }
 
 func main() {
@@ -21,5 +41,6 @@ func main() {
 	m['D'] = 'B'
 	m['E'] = 'D'
 	m['F'] = 'E'
+	fmt.Println(m)
 	fmt.Println(report(m))
 }
