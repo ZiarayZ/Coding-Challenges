e6714e5db4d800cb03db31d8766e226b0bf5c953
ZiarayZ <jakesmith@binaryrage.com>
2022-08-18 10:55:15 +0100

    recursive largest palindrome
    - cut string for every largest palindrome till every substring is a palindrome, then count how many substrings-1 there are for how many cuts

--- Golang/Partition/partition.go
+++ Golang/Partition/partition.go
@@ -8,13 +8,62 @@ import (
 	"strings"
 )
 
+func cut(B string) []string {
+	n := len([]rune(B))
+	//if the string is a single letter return that
+	if n == 1 {
+		return []string{B}
+	}
+
+	//declare vars
+	maxLength := 1
+	start := 0
+	flag := true
+
+	//double loop to compare runes
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			flag = true
+
+			for k := 0; k < ((j-i)/2)+1; k++ {
+				if B[i+k] != B[j-k] {
+					flag = false
+				}
+			}
+
+			//if largest palindrome is the entire input just return the input
+			if i == 0 && j+1 == n {
+				return []string{B}
+			}
+
+			//record last valid largest palindrome index
+			if flag && (j-i+1) > maxLength {
+				start = i
+				maxLength = j - i + 1
+			}
+		}
+	}
+	//cut out largest palindrome and return it
+	returnSlice := []string{B[start : start+maxLength]}
+	//repeat process for before and after largest palindrome
+	if start > 0 {
+		firstSlice := cut(B[0:start])
+		returnSlice = append(returnSlice, firstSlice...)
+	}
+	if start+maxLength < n {
+		lastSlice := cut(B[start+maxLength:])
+		returnSlice = append(returnSlice, lastSlice...)
+	}
+	//return all cut palindromes
+	return returnSlice
+}
+
 func partite(A string) ([]string, int) {
 	//removes whitespace in and between for palindromes
 	A = strings.TrimSpace(A)
 	A = strings.ReplaceAll(A, " ", "")
-	srune := []rune(A)
-	returnSlice := []string{string(srune[0]), string(srune[1:])}
-	return returnSlice, 1
+	sliced := cut(A)
+	return sliced, len(sliced) - 1
 }
 
 func main() {
