a043fb0cc9b326ca19bcc1f83d9a0538da89ddf8
ZiarayZ <jakesmith@binaryrage.com>
2022-08-19 15:51:03 +0100

    finish rotate method check

--- Golang/Rotations/rotations.go
+++ Golang/Rotations/rotations.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func countDigits(n int) int {
@@ -16,10 +17,33 @@ func countDigits(n int) int {
 }
 
 func rotate(num1 int, num2 int) bool {
+	number1 := num1
+	number2 := num2
 	digits1 := countDigits(num1)
 	digits2 := countDigits(num2)
+	pow1 := int(math.Pow10(digits1 - 1))
+	pow2 := int(math.Pow10(digits2 - 1))
 
-	return true
+	//if num1 has less digits than num2 then this loop will never return true
+	for i := 0; i < digits1-1 && digits1 >= digits2; i++ {
+		firstDigit := num1 / pow1
+		left := ((num1 * 10) + firstDigit) - (firstDigit * pow1 * 10)
+		num1 = left
+		if num1 == number2 {
+			return true
+		}
+	}
+	//if num2 has less digits than num1 then this loop will never return true
+	for i := 0; i < digits2-1 && digits1 <= digits2; i++ {
+		firstDigit := num2 / pow2
+		left := ((num2 * 10) + firstDigit) - (firstDigit * pow2 * 10)
+		num2 = left
+		if number1 == num2 {
+			return true
+		}
+	}
+
+	return false
 }
 
 func recycle(nums []int) int {
