0b05feb5ba3c771fc48e24684bc16a75e2f10f6c
ZiarayZ <jakesmith@binaryrage.com>
2022-08-23 11:38:34 +0100

    setup

--- Golang/Routes/route.go
+++ Golang/Routes/route.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+func calculate(A []int, B []int) {
+
+}
+
 func main() {
-	fmt.Println("")
+	A := []int{14, 5, 12, 19, 11, 2, 18, 20, 4, 3, 8, 15, 7, 17, 10, 9, 16, 1, 13, 6}
+	B := []int{5, 14, 15, 2, 19, 9, 11, 1, 6, 12, 13, 17, 20, 3, 4, 16, 18, 10, 7, 8}
+	fmt.Println(calculate(A, B))
 }
