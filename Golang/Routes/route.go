cd431b79b416244c2ce86395ef45cc9bf897a322
ZiarayZ <jakesmith@binaryrage.com>
2022-08-23 12:12:51 +0100

    fix/second attempt

--- Golang/Routes/route.go
+++ Golang/Routes/route.go
@@ -9,16 +9,17 @@ func calculate(A []int, B []int, gas int, pos int, loc int) (int, bool) {
 	N2 := len(B)
 	if N != N2 {
 		return pos, false
-	} else if pos == loc {
-		return loc, true
 	} else if pos >= N {
-		pos = 0
+		pos = -1
 	}
+	pos++
 	gas += A[pos]
 	fmt.Println(gas, B[pos])
+	if pos == loc {
+		return pos, true
+	}
 	if gas >= B[pos] {
 		gas -= B[pos]
-		pos++
 		return calculate(A, B, gas, pos, loc)
 	} else {
 		return pos, false
@@ -27,8 +28,9 @@ func calculate(A []int, B []int, gas int, pos int, loc int) (int, bool) {
 }
 
 func main() {
+	//random set generator
 	A := []int{14, 5, 19, 12, 11, 3, 18, 20, 4, 3, 8, 15, 7, 17, 10, 9, 16, 13, 1, 6}
 	B := []int{5, 14, 2, 15, 19, 9, 11, 1, 6, 12, 13, 17, 3, 20, 4, 16, 18, 10, 7, 8}
-	location, ok := calculate(A, B, 0, 0, 0)
+	location, ok := calculate(A, B, 0, -1, 0) //pos is 1 less than the actual location, because you haven't filled up there yet
 	fmt.Println(location, ok)
 }
