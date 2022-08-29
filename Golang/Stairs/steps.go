ab4e37a2b7cb06800b039faa946c224c6aa5b086
ZiarayZ <jakesmith@binaryrage.com>
2022-08-29 14:43:33 +0100

    first failed attempt
    - works for minimum cases of n = 1, 2 or 3

--- Golang/Stairs/steps.go
+++ Golang/Stairs/steps.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+//climb 3 steps, or 2-1 or 1-2 or 1-1-1	= 4
+//climb 2 steps, or 1-1					= 2
+//climb 1 steps							= 1
+func climb(n int) int {
+	//return exact combos below 3 steps
+	if n == 3 {
+		return 4
+	} else if n == 2 {
+		return 2
+	} else if n == 1 {
+		return 1
+	}
+
+	//calculate every 3 steps is 4 combos
+	expo := n / 3
+	value := int(math.Pow(4, float64(expo)))
+	//add the remainder after all the 3 steps
+	if n%3 == 2 {
+		value *= 2
+	} else if n%3 == 1 {
+		value++
+	}
+
+	return value
+}
+
 func main() {
-	fmt.Println("")
+	steps := 10
+	fmt.Println("There are", climb(steps), "ways to climb", steps, "steps.")
 }
