b46e9e798dce2a9cce7dca6a2ded698f4b194d14
ZiarayZ <jakesmith@binaryrage.com>
2022-08-29 17:00:20 +0100

    fix issues with original again

--- Golang/Stairs/steps.go
+++ Golang/Stairs/steps.go
@@ -25,10 +25,10 @@ func climb(n int) int {
 	//the remainder causes issues
 	if n%3 == 2 {
 		//TODO: introduce the 5 step method check
-		value += (expo + 1) * 2
+		value *= (expo + 1) * 2
 	} else if n%3 == 1 {
 		//TODO: introduce the 4 step method check
-		value += expo + 1
+		value *= expo + 1
 	}
 
 	return value
