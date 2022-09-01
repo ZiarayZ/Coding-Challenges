19aa46ea8f9eaff2b37f8b4fed963710e275f1a3
ZiarayZ <jakesmith@binaryrage.com>
2022-09-01 16:52:34 +0100

    solution made
    - included length of slice in return of splitInt

--- Golang/Palindrome/palindrome.go
+++ Golang/Palindrome/palindrome.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-func splitInt(n int) []int {
+func splitInt(n int) ([]int, int) {
 	slice := []int{}
 	//loop and divide by 10 getting each digit with each division
 	//due to rounding down just use "n > 0" as loop condition
-	for ; n > 0; n /= 10 {
+	for ; n != 0; n /= 10 {
 		slice = append(slice, n%10)
 	}
-	return slice
+	return slice, len(slice)
 }
 
 func paliCheck(n int) bool {
@@ -21,10 +21,17 @@ func paliCheck(n int) bool {
 	}
 
 	//otherwise split the digits and check
-	slice := splitInt(n)
-	return len(slice) <= 1
+	slice, num := splitInt(n)
+	for i := 0; i < (num+num%2)/2; i++ {
+		if slice[i] != slice[num-1-i] {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
+	//check both possible cases
 	fmt.Println(paliCheck(9), paliCheck(45654))
+	//ignoring negative cases, as they are treated the same as positive ones
 }
