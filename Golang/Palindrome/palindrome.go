c0a31e43914a7a2c73cc9d3878bc967c5b946e78
ZiarayZ <jakesmith@binaryrage.com>
2022-09-01 16:32:02 +0100

    setup for the solution

--- Golang/Palindrome/palindrome.go
+++ Golang/Palindrome/palindrome.go
@@ -4,6 +4,27 @@ import (
 	"fmt"
 )
 
+func splitInt(n int) []int {
+	slice := []int{}
+	//loop and divide by 10 getting each digit with each division
+	//due to rounding down just use "n > 0" as loop condition
+	for ; n > 0; n /= 10 {
+		slice = append(slice, n%10)
+	}
+	return slice
+}
+
+func paliCheck(n int) bool {
+	//single digit is a palindrome, base case
+	if n < 10 && n > -10 {
+		return true
+	}
+
+	//otherwise split the digits and check
+	slice := splitInt(n)
+	return len(slice) <= 1
+}
+
 func main() {
-	fmt.Println("")
+	fmt.Println(paliCheck(9), paliCheck(45654))
 }
