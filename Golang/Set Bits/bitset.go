05d6946917acc4f1052ba500ec2f1fa7502ba0b3
ZiarayZ <jakesmith@binaryrage.com>
2022-09-01 12:07:46 +0100

    finish task using iteration

--- Golang/Set Bits/bitset.go
+++ Golang/Set Bits/bitset.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+func countSetBits(n int) int {
+	value := 0
+	//calculate the exponent (x) of 2 that fits into n: 2^x = n, then add 1
+	iterat := int(math.Log(float64(n))/math.Log(2)) + 1
+	//iterate through every possible bit that could be set
+	for i := 0; i < iterat; i++ {
+		value += (n >> i) & 1
+	}
+	return value
+}
+
 func main() {
-	fmt.Println("")
+	val := 31
+	fmt.Println("There are", countSetBits(val), "set bits in the int:", val)
 }
