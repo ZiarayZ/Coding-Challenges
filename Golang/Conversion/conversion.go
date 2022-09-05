131598ffa3a4442ee322a23036d671a3645c5ca7
ZiarayZ <jakesmith@binaryrage.com>
2022-09-05 11:42:59 +0100

    finished bin-dec conversion

--- Golang/Conversion/conversion.go
+++ Golang/Conversion/conversion.go
@@ -5,7 +5,17 @@ import (
 )
 
 func toDecimal(val string) int {
-	return 0
+	r := []rune(val)
+	newVal := 0
+	//reverse loop through string
+	for i, j := len(r)-1, 1; i >= 0; i-- {
+		//hit a one, add 'j' which doubles each loop: 1 2 4 8 16 32 64
+		if r[i] == '1' {
+			newVal += j
+		}
+		j *= 2
+	}
+	return newVal
 }
 
 func toBinary(val int) string {
