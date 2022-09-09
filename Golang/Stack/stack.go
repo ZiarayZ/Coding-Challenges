1d40121772fef2b67c457176983fcd01a1f44d1c
ZiarayZ <jakesmith@binaryrage.com>
2022-09-09 10:30:47 +0100

    comments

--- Golang/Stack/stack.go
+++ Golang/Stack/stack.go
@@ -4,26 +4,31 @@ import (
 	"fmt"
 )
 
+// item on stack
 type Node struct {
 	value interface{}
 	prev  *Node
 }
 
+//stack itself
 type Stack struct {
 	last    *Node
 	minimum *Node
 }
 
+//stack methods
 func (s *Stack) min() interface{} {
 	return s.minimum.value
 }
 func (s *Stack) pop() interface{} {
 	toRet := s.last.value
+	//set last to new last, memory loss?
 	s.last = s.last.prev
 	return toRet
 }
 func (s *Stack) push(val interface{}) bool {
 	s.last = &Node{value: val, prev: s.last}
+	//only checks for int values, ignored for any other value
 	testVal, valOk := val.(int)
 	testMin, minOk := s.minimum.value.(int)
 	if valOk && minOk && testVal <= testMin {
@@ -35,6 +40,7 @@ func (s *Stack) push(val interface{}) bool {
 
 func main() {
 	testStack := Stack{}
+	//test methods
 	testStack.push(5)
 	fmt.Println(testStack.pop())
 	fmt.Println(testStack.min())
