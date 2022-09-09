59412ba638fa76e6014908b5d633e06aef024d50
ZiarayZ <jakesmith@binaryrage.com>
2022-09-09 14:45:50 +0100

    fix coordinates being only positive
    -includes negative axis now as well

--- Golang/Z_Order/Z_order.go
+++ Golang/Z_Order/Z_order.go
@@ -12,30 +12,30 @@ import (
 //byte size of 4 or 8 depending on system, so will restrict to 4 for 32-bit systems
 //using unsigned ints for bit operations, 2 16 bits become a 32 bit
 // coords -> single value
-func interleave(coords [2]uint16) uint32 {
-	var num uint32 = 0
-	var i uint16 = 0
+func interleave(coords [2]int16) int32 {
+	var num int32 = 0
+	var i int16 = 0
 	for i = 0; i < 16; i++ { //loop could be made more efficient later, break after checking all set bits
-		num |= uint32(coords[0]&(1<<i)) << i
-		num |= uint32(coords[1]&(1<<i)) << (i + 1)
+		num |= int32(coords[0]&(1<<i)) << i
+		num |= int32(coords[1]&(1<<i)) << (i + 1)
 	}
 	return num
 }
 
 // single value -> coords
-func recall(value uint32) [2]uint16 {
-	coords := [2]uint16{0, 0}
-	var i uint16 = 0
+func recall(value int32) [2]int16 {
+	coords := [2]int16{0, 0}
+	var i int16 = 0
 	for i = 0; i < 16; i++ {
-		coords[0] |= uint16((value >> (i)) & (1 << i))
-		coords[1] |= uint16((value >> (i + 1)) & (1 << i))
+		coords[0] |= int16((value >> (i)) & (1 << i))
+		coords[1] |= int16((value >> (i + 1)) & (1 << i))
 	}
 	return coords
 }
 
 func main() {
-	slice := [2]uint16{127, 127}
+	slice := [2]int16{127, 127}
 	fmt.Println(interleave(slice))
-	var number uint32 = 16383
+	var number int32 = 16383
 	fmt.Println(recall(number))
 }
